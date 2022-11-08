f406202124117add4cbcf93a0c38724d0112349b
Xing Zhao <zhaoxing8325@gmail.com>
2022-11-07 21:53:11 -0800

    Initial Commit

---
+++ CircleAndSlider/CircleAndSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAndSliderApp.swift
+//  CircleAndSlider
+//
+//  Created by XING ZHAO on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct CircleAndSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
