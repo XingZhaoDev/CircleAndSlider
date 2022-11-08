7217d0dd7335401234e30f1803292d6708f2c8b6
Xing Zhao <zhaoxing8325@gmail.com>
2022-11-08 11:01:00 -0800

    code

--- CircleAndSlider/ContentView.swift
+++ CircleAndSlider/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var insertAge: CGFloat = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: insertAge / 100.0)
+                    .stroke(style: StrokeStyle(lineWidth: 10))
+                    .padding()
+                HStack {
+                    Text("Value:")
+                    Text("\(insertAge, specifier: "%.0f")%")
+                        .animation(.easeInOut, value: insertAge)
+                }
+                .font(.title)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Slider(value: $insertAge, in: 0...100, step: 1)
+                .padding()
         }
-        .padding()
+        
     }
 }
 
